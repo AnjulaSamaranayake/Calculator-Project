7dffd72eb5b59bbb38b3928f63fd9419302b5d17
AnjulaSamaranayake <anjulasankalpa2255@gmail.com>
2024-07-29 16:41:26 +0530

    erros found

---
+++ cal.js
@@ -0,0 +1,7 @@
+var tot1;
+
+function office() {
+    var num1 = document.getElementById("off-exe-val").value;
+    console.log(getElementById("off-exe-val").value)
+    document.getElementById("off-exe-sub") = 300 * num1 ;
+}
